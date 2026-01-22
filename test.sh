ba5afa575ee4d816631a98eb09d7ffb96b4abbb0
Baxter Rogers <baxter.rogers@vumc.org>
2026-01-22 15:30:05 -0600

    tester

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 docker run \
     --mount type=bind,src=$(pwd -P)/INPUTS,dst=/INPUTS \
     --mount type=bind,src=$(pwd -P)/OUTPUTS,dst=/OUTPUTS \
+    --mount type=bind,src=$(pwd -P)/freesurfer_license.txt,dst=/opt/freesurfer/license.txt \
     pennlinc/aslprep:25.1.0 \
     /OUTPUTS/aslBIDS \
     /OUTPUTS/results \
