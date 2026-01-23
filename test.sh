6ae14ef797b599518cf7dc5719bae5bf992cc377
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2026-01-22 19:14:33 -0600

    tester

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ docker run \
     --mount type=bind,src=$(pwd -P)/OUTPUTS,dst=/OUTPUTS \
     --mount type=bind,src=$(pwd -P)/freesurfer_license.txt,dst=/opt/freesurfer/license.txt \
     pennlinc/aslprep:25.1.0 \
+    --fs-subjects-dir /INPUTS/freesurfer720 \
     /OUTPUTS/aslBIDS \
     /OUTPUTS/results \
     participant
