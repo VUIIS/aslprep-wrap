1e282ee1b30d723bd446bb1da1df02847b8a2e95
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2026-02-05 08:37:27 -0600

    tester

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ docker run \
     --mount type=bind,src=$(pwd -P)/OUTPUTS,dst=/OUTPUTS \
     --mount type=bind,src=$(pwd -P)/freesurfer_license.txt,dst=/opt/freesurfer/license.txt \
     pennlinc/aslprep:25.1.0 \
-    --output-spaces T1w MNI152NLin2009cAsym \
+    --output-spaces asl T1w MNI152NLin2009cAsym \
     --fs-subjects-dir /INPUTS/freesurfer720 \
     --basil \
     /OUTPUTS/aslBIDS \
